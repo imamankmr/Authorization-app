75027b660d4e4003abec43a68e4ec8b47bd6fda8
imamankmr <amanjnvr@gmail.com>
2024-10-29 01:36:10 +0530

    third-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import SignUp from './Component/SignUp';
-import Login from './Component/Login';
-import Home from './Component/Home';
+import React, { useState } from "react";
+import { useSelector }from 'react-redux'
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import SignUp from "./Component/SignUp";
+import Login from "./Component/Login";
+import Dashboard from "./Component/Dashboard";
 
 const App = () => {
-  return (
-   <>
-   
-   </>
-  );
-}
+  const isAuthenticated = useSelector((state)=> state.auth.isAuthenticated);
+  const [isLogin,setisLogin]=useState(true)
+  const toggleForm=()=>{
+    setisLogin(!isLogin)
+  }
+  if (isAuthenticated) {
+    return <Dashboard />;
+  }
+  return 
+  <>
+    {isLogin?<Login/>:<SignUp/>}
+    <button onClick={toggleForm}>
+   {isLogin?'need an signup':'login'}
+    </button>
+  </>;
+};
 
 export default App;
--- src/Component/Dashboard.jsx
+++ src/Component/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Dashboard from './Dashboard';
+
 
 const Dashboard = () => {
   return (
